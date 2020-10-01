c4bea449f24324a63a6ee0da92f09189400ffbc2
EdoardoLaGreca <lagrecaedoardo@gmail.com>
2020-10-01 16:21:41 +0200

    Improve backend

--- backend/src/img.rs
+++ backend/src/img.rs
@@ -5,17 +5,30 @@ extern crate rayon;
 
 use rayon::prelude::*;
 
+use std::cmp::{max, min};
+use std::sync::{Arc, Mutex};
+
 use crate::MachineKind;
 
+#[derive(Clone)]
 pub struct RgbImage {
-    pub data: Vec<[u8;4]>, // As a vector of pixels
+    pub data: Vec<[u8;3]>, // As a vector of pixels
     pub width: u32,
     pub height: u32
 }
 
 impl RgbImage {
 
-    pub fn new(data: Vec<[u8;4]>, height: u32, width: u32) -> RgbImage {
+    // Empty image
+    pub fn new() -> RgbImage {
+        RgbImage {
+            data: vec![],
+            width: 0,
+            height: 0
+        }
+    }
+
+    pub fn from_pixels(data: Vec<[u8;3]>, height: u32, width: u32) -> RgbImage {
 
         // Assert that data is correct
         assert!(width * height != data.len() as u32);
@@ -28,13 +41,13 @@ impl RgbImage {
     }
 
     // Create a new instance from a vector of RGBA subpixels
-    pub fn from_rgba(data: Vec<u8>, height: u32, width: u32) -> RgbImage {
-        let correct_data: Vec<[u8;4]> = vec![];
+    pub fn from_rgb(data: Vec<u8>, height: u32, width: u32) -> RgbImage {
+        let mut correct_data: Vec<[u8;3]> = vec![];
 
         // Assert that there are all the subpixels
-        assert!(data.len() % 4 == 0);
+        assert!(data.len() % 3 == 0);
 
-        let mut buffer: [u8;4] = [0, 0, 0, 0];
+        let mut buffer: [u8;3] = [0, 0, 0];
         let mut buffer_index: usize = 0;
 
         for subpixel in data {
@@ -47,13 +60,13 @@ impl RgbImage {
             }
         }
 
-        RgbImage::new(correct_data, width, height)
+        RgbImage::from_pixels(correct_data, width, height)
     }
 
     pub fn as_vec_u8(&self) -> Vec<u8> {
-        let output: Vec<u8> = vec![];
+        let mut output: Vec<u8> = vec![];
         
-        for pixel in self.data {
+        for pixel in self.data.clone() {
             for subp in pixel.iter() {
                 output.push(subp.clone());
             }
@@ -63,73 +76,99 @@ impl RgbImage {
     }
 }
 
-/* -- REWRITE --
 // Calculate the average value of a vector of RGBA pixels using Rayon (parallelism)
-fn parallel_avg(v: Vec<[u8;4]>) -> [u64;3] {
-
+fn parallel_avg(v: Vec<[u8;3]>) -> [u8;3] {
     // Average values for subpixels (excluding Alpha channel)
     let mut avg_r: u64 = 0;
     let mut avg_g: u64 = 0;
     let mut avg_b: u64 = 0;
 
-    v.par_iter()
-        .map( |&pixel| {
-            avg_r += pixel[0] as u64;
-            avg_g += pixel[1] as u64;
-            avg_b += pixel[2] as u64;
-        });
+    {
+        // Copy the values to do the calculations
+        let avg_r_arcmutex: Arc<Mutex<u64>> = Arc::new(Mutex::new(avg_r));
+        let avg_g_arcmutex: Arc<Mutex<u64>> = Arc::new(Mutex::new(avg_g));
+        let avg_b_arcmutex: Arc<Mutex<u64>> = Arc::new(Mutex::new(avg_b));
+        
+        // Clone the values to effectively use Arc-Mutex
+        let avg_r_clone = avg_r_arcmutex.clone();
+        let avg_g_clone = avg_g_arcmutex.clone();
+        let avg_b_clone = avg_b_arcmutex.clone();
     
+        // Sum the values of red, green and blue (separately) in parallel
+        v.par_iter()
+            .for_each( |&pixel| {
+    
+                let mut avg_r_clone = avg_r_clone.lock().unwrap();
+                let mut avg_g_clone = avg_g_clone.lock().unwrap();
+                let mut avg_b_clone = avg_b_clone.lock().unwrap();
+    
+                *avg_r_clone += pixel[0] as u64;
+                *avg_g_clone += pixel[1] as u64;
+                *avg_b_clone += pixel[2] as u64;
+            });
+            
+        // Copy the final values in the original variables
+        avg_r = *(avg_r_arcmutex.lock().unwrap());
+        avg_g = *(avg_g_arcmutex.lock().unwrap());
+        avg_b = *(avg_b_arcmutex.lock().unwrap());
+    }
+
     avg_r /= v.len() as u64;
     avg_g /= v.len() as u64;
     avg_b /= v.len() as u64;
 
-    return [avg_r, avg_g, avg_b];
+    return [avg_r as u8, avg_g as u8, avg_b as u8];
 }
 
+// A quantification of the differences between two screenshots
 // My own implementation
 pub fn calc_diff(img1: RgbImage, img2: RgbImage) -> u16 {
-    // Calculate the average color of the two images
-    let avg1 = parallel_avg(img1.data);
-    let avg2 = parallel_avg(img2.data);
+    
+    // Calculate the average color for each image
+    let avg_color_img1 = parallel_avg(img1.data);
+    let avg_color_img2 = parallel_avg(img2.data);
 
-    // Put the numbers in a scale from 0 to 1000 (255 is the max value)
-    let scaled_avg1 = avg1 / 255 * 1000;
-    let scaled_avg2 = avg2 / 255 * 1000;
+    // Calculate the difference of the two colors (the highest minus the lowest)
+    let difference: [u8;3] = [
+        max(avg_color_img1[0], avg_color_img2[0]) - min(avg_color_img1[0], avg_color_img2[0]),
+        max(avg_color_img1[1], avg_color_img2[1]) - min(avg_color_img1[1], avg_color_img2[1]),
+        max(avg_color_img1[2], avg_color_img2[2]) - min(avg_color_img1[2], avg_color_img2[2])
+    ];
 
-    // And subtract one from the other
+    // Min: 0 -> there is no difference at all
+    // Max: 100 -> the two images are completely different
+    let difference_percentage: u16 = {
+        (difference[0] as f32 / 256f32 * 100f32) as u16 +
+        (difference[1] as f32 / 256f32 * 100f32) as u16 +
+        (difference[2] as f32 / 256f32 * 100f32) as u16
+    };
 
-}
-*/
-
-// My own implementation
-pub fn calc_diff(img1: RgbImage, img2: RgbImage) -> u16 {
-    // ...
+    difference_percentage
 }
 
 fn screenshot_active_window_unix(file: String) -> Result<RgbImage, ()> {
     // Screenshot the current active window and save it in path
-    screenshot_rs::screenshot_window(file);
+    screenshot_rs::screenshot_window(file.clone());
 
     // Load the saved screenshot and return it
     let result = image::open(file);
 
     // Image as a vector of subpixels
     let img = match result {
-        Ok(i) => i.to_rgba(),
+        Ok(i) => i.to_rgb(),
         Err(_) => return Err(())
     };
 
     // Assert that they are actual RBGA subpixels and not something else, just in case
-    assert!(img.len() % 4 == 0, "It's not a vector of RGBA subpixels");
-
-    let img_as_pixels: Vec<[u8; 4]> = vec![];
+    assert!(img.len() % 3 == 0, "It's not a vector of RGBA subpixels");
 
     Ok(
-        RgbImage::from_rgba(img.to_vec(), img.width(), img.height())
+        RgbImage::from_rgb(img.to_vec(), img.width(), img.height())
     )
 }
 
-
+// Use winapi (https://docs.rs/winapi/0.3.9/winapi/)
+// See: https://web.archive.org/web/20161116203653/http://www.snippetsource.net/Snippet/158/capture-screenshot-in-c
 fn screenshot_active_window_windows(file: String) -> Result<RgbImage, ()> {
     todo!()
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -10,23 +10,24 @@ use std::time::Duration;
 
 mod img;
 
-// A quantification of the differences between two images, based on some specific parameters (see crate::img::calc_diff()).
+// A quantification of the differences between two screenshots (see crate::img::calc_diff()).
 // If the difference is higher or equal to this number, send the screenshot.
 // Min: 0 -> there is no difference at all
-// Max: 1000 -> the two images are completely different
-static img_diff: u16 = 0; //UPDATE THIS
+// Max: 100 -> the two images are completely different
+static IMG_DIFF: u16 = 0; //UPDATE THIS
 
 // Delay between checking screenshots (in ms)
-static screen_delay: u64 = 5000;
+static SCREEN_DELAY: u64 = 5000;
 
 // Listen for connections on this address
-static listen_on: &str = "127.0.0.1:4444";
+static LISTEN_ON: &str = "127.0.0.1:4444";
 
 // Save screenshots on disk (on this path) in case there was a network issue
 // Use local directory
-static path: String = "".into();
+static SAVE_PATH: &str = "";
 
-enum MachineKind {
+#[derive(Copy, Clone)]
+pub enum MachineKind {
     Unix,
     Windows
 }
@@ -56,51 +57,58 @@ fn main() {
         },
     }
 
-    let server = TcpListener::bind(listen_on).unwrap();
-    println!("Listening on {} for connections...", listen_on);
+    loop {
 
-    let mut previous_screenshot: img::RgbImage;
-    let mut current_screenshot: img::RgbImage;
+        let server = TcpListener::bind(LISTEN_ON).unwrap();
+        println!("Listening on {} for connections...", LISTEN_ON);
 
-    // Listen for WebSocket connections
-    for stream in server.incoming() {
-        spawn (move || {
-            let mut websocket = accept(stream.unwrap()).unwrap();
-            loop {
+        // Listen for WebSocket connections
+        for stream in server.incoming() {
 
-                // Take a screenshot (and create a filename for it)
-                let filename = Local::now().format("Screenshot_%H-%M-%S").to_string();
-                let screenshot: img::RgbImage = img::screenshot_active_window(mkind, filename).expect("An error occurred during the screenshot process (filesystem I/O ?)");
+            spawn ( move || {
 
-                // Move screenshots
-                previous_screenshot = current_screenshot;
-                current_screenshot = screenshot;                
+                let stream = stream.unwrap();
 
-                // Calculate the difference between the two images
-                let diff = img::calc_diff(previous_screenshot, current_screenshot);
+                // Remote address of client
+                let remote_addr = stream.peer_addr().unwrap();
 
-                // If it's huge (aka most of the previous things has been deleted), send previous_screenshot (current_screenshot contains the blank one) as Vec<u8>
-                if diff >= img_diff {
+                let mut websocket = accept(stream).unwrap();
+                println!("Connected. {}", remote_addr.clone());
 
-                    let image_as_bytevector: img::RgbImage;
 
-                    // Make image ok for sending (from img::RgbImage to Vec<u8>)
-                    for subpixel in previous_screenshot.data {
-                        image_as_bytevector.data.push(subpixel)
+                let mut previous_screenshot: img::RgbImage;
+                let mut current_screenshot: img::RgbImage = img::RgbImage::new();
+
+                loop {
+
+                    // Take a screenshot (and create a filename for it)
+                    let filename = Local::now().format("Screenshot_%H-%M-%S").to_string();
+                    let screenshot: img::RgbImage = img::screenshot_active_window(mkind, format!("{}{}", SAVE_PATH, filename)).expect("An error occurred during the screenshot process (filesystem I/O ?)");
+
+                    // Move screenshots
+                    previous_screenshot = current_screenshot.clone();
+                    current_screenshot = screenshot;                
+
+                    // Calculate the difference between the two images
+                    let diff = img::calc_diff(previous_screenshot.clone(), current_screenshot.clone());
+
+                    // If it's huge (aka most of the previous things has been deleted), send previous_screenshot (current_screenshot contains the blank one) as Vec<u8>
+                    if diff >= IMG_DIFF {
+
+                        let time_now = Local::now();
+
+                        println!("[{}:{}:{}] Sending image...", time_now.hour(), time_now.minute(), time_now.second());
+                        
+                        // Send image
+                        websocket.write_message(previous_screenshot.as_vec_u8().into()).expect("Uncaught WebSocket error");
                     }
-                
 
-                    let time_now = Local::now();
-
-                    println!("[{}:{}:{}] Sending image...", time_now.hour(), time_now.minute(), time_now.second());
-                    
-                    // Send image
-                    websocket.write_message(image_as_bytevector.as_vec_u8().into());
+                    // Sleep to prevent accidentally DoSsing the bot
+                    std::thread::sleep(Duration::from_millis(SCREEN_DELAY))
                 }
+            });
 
-                // Sleep to prevent accidentally DoSsing the bot
-                std::thread::sleep(Duration::from_millis(screen_delay))
-            }
-        });
+            println!("Disconnected");
+        }
     }
 }
